ec27acad0199d6153619f626ec518d4c20c918c3
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-15 23:41:28 +0400

    Remove unused useEffect import

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { supabase } from '../supabaseClient';
 import { Button } from '../styles';
