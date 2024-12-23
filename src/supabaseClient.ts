5c11b8ec7b59d549be5cd5579597244ef1c8a84e
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-23 17:24:39 +0400

    Update Supabase URL and key

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://loyzwjzsjnikmnuqilmv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxveXp3anpzam5pa21udXFpbG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NzY2OTMsImV4cCI6MjA0OTE1MjY5M30.vwXqtGwm4SG1juWZ_YxMhhZZSiQ7TbtUcB77h6wNYus';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
