4db6b07b180c700069aeb8da98aa03efa5e2e5e9
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-30 22:47:26 +0400

    Fix routing configuration for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/class-manager-.">
+    <BrowserRouter basename="/class-manager">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
