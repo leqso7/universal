526d208e75adb1a4ec6480ab271752a5097c3916
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-31 17:26:28 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/class-manager/',
+  base: '/leqso7-class-manager-web/',
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +17,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/class-manager/',
-        scope: '/class-manager/',
+        start_url: '/leqso7-class-manager-web/',
+        scope: '/leqso7-class-manager-web/',
         icons: [
           {
             src: 'pwa-192x192.png',
