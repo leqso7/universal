2f823b9cda20ac436935a105db255f8088e4a054
leqso7 <115240888+leqso7@users.noreply.github.com>
2025-01-12 20:39:44 +0400

    Add Vercel configuration and dynamic base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/class-manager-web/',
+  base: process.env.VERCEL ? '/' : '/class-manager-web/',
   plugins: [
     react(),
     VitePWA({
