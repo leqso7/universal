68ae55aa177560d54bc5d4908f6e2b97272c5513
leqso7 <leqso7@users.noreply.github.com>
2025-03-11 11:54:22 +0000

    deploy: cca49ad0a33c52248630bde6967d0fd8a3711a0f

--- assets/AttentionGame-CjWyvPmz.js
+++ assets/AttentionGame-CjWyvPmz.js
@@ -1,4 +1,4 @@
-import{d,r as l,u as J,a as K,j as s,H as Q}from"./index-BEG99lLj.js";const U=d.div`
+import{d,r as l,u as J,a as K,j as s,H as Q}from"./index-Bk4ltbmF.js";const U=d.div`
   position: fixed;
   inset: 0;
   margin: 0;
--- assets/NameModal-BoXbEoOV.js
+++ assets/NameModal-BoXbEoOV.js
@@ -1,4 +1,4 @@
-import{d as o,r as s,j as t}from"./index-BEG99lLj.js";const d=o.div`
+import{d as o,r as s,j as t}from"./index-Bk4ltbmF.js";const d=o.div`
   position: fixed;
   top: 0;
   left: 0;
